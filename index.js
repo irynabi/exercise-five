88cc2d104b11a2a73fb260bccbfe88525b6db799
Iryn Abigail Delim <ipd5759@nyu.edu>
2023-11-07 09:30:16 -0500

    firebase installed

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
+const firebase = require("firebase/app");
 const port = 3000
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC9T6uCWcxYwB2juyWXkk73YDeJSMTpZQo",
+    authDomain: "exercise-five-e60da.firebaseapp.com",
+    projectId: "exercise-five-e60da",
+    storageBucket: "exercise-five-e60da.appspot.com",
+    messagingSenderId: "364361531975",
+    appId: "1:364361531975:web:0c6ca208d4165fa9a92762"
+  };
+
+firebase.initializeApp(firebaseConfig);
 
 const indexRoute = require('./routes/index');
 //const singlePostRoute = require('./routes/singlePost');
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const firestore = require("firebase/firestore");
+const db = firestore.getFirestore();
 
 router.get("/", (req, res) => {
-    res.send("All posts!");
-});
+    const postsQuery = firestore.getDocs(firestore.collection(db, "posts"));
+    const postsArray = []
+
+    postsQuery.then((response) => {
+            response.forEach((post) =>{
+                postsArray.push({id:post.id, ...post.data() });
+            });
+            res.send(postsArray);
+            })
+            .catch((error) =>{
+                return res.send(error);
+            });
+    });
+
 
 module.exports = router;
