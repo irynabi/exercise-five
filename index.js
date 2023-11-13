b7025fbbaf1e15ae088de025ed97ed5d7fc1b8f3
Iryn Abigail Delim <ipd5759@nyu.edu>
2023-11-13 16:57:12 -0500

    updated

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const indexRoute = require('./routes/index');
-//const singlePostRoute = require('./routes/singlePost');
+const singlePostRoute = require('./routes/singlePost');
 //const createPostRoute = require('./routes/createPost');
 
 app.use("/", indexRoute);
+app.use("/post", singlePostRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
